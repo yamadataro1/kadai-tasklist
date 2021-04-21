3400c47bcd9c5e3d4734b95d02fd29b183f9665b
taro.yamada <lakjsdfg;lajksd4fmdk;234@gmail.com>
2021-04-21 23:05:55 +0900

    Add getAlltasks

--- src/models/taskDTO.java
+++ src/models/taskDTO.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tasklists")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM taskDTO AS m ORDER BY m.id DESC"
+            )
+})
+@Table(name = "tasks")
 public class taskDTO {
 
     @Id
@@ -24,6 +32,9 @@ public class taskDTO {
 
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
+
+
+
 
     public Integer getId() {
         return id;
@@ -51,16 +62,6 @@ public class taskDTO {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    @Column(name = "updated_at", nullable = false)
-    private Timestamp updated_at;
 
 
 }
