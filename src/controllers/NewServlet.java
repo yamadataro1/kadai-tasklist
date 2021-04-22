23f4f0b7ebd4a57cad739fd022ca9bd2a9cd44b3
taro.yamada <lakjsdfg;lajksd4fmdk;234@gmail.com>
2021-04-22 17:08:00 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.taskDTO;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +28,33 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        EntityManager em = DBUtil.createEntityManager();
+
+       // TaskDTOのインスタンスを生成
+        taskDTO t = new taskDTO();
+
+       // tの各フィールドにデータを代入
+
+        String content = "hello";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
+
+     // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
+}
 }
